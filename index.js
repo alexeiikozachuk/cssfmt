04338c21759ee4d4235b29dfff6aaef0c7d13049
Masaaki Morishita <ikasama3.0@gmail.com>
2015-08-16 21:49:47 +0900

    wip

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var postcss = require('postcss')
+var editorconfig = require('editorconfig')
+var syncexec = require('sync-exec')
+var confPath = process.cwd() + '/.editorconfig'
+
+/*
+var conf
+editorconfig.parse(confPath).then(function (data) {
+  conf = data
+})
+*/
+
 var formatAtRules = require('./lib/formatAtRules')
 var formatRules = require('./lib/formatRules')
 var formatSassVariables = require('./lib/formatSassVariables')
 
 var cssfmt = postcss.plugin('cssfmt', function () {
+
   return function (root) {
 
     formatAtRules(root)
@@ -13,6 +25,7 @@ var cssfmt = postcss.plugin('cssfmt', function () {
     return root
   }
 })
+
 
 module.exports = cssfmt
 
